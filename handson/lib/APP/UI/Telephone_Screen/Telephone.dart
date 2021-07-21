4421e4bc3cf7c132c73f5c9caf8f6f339190cef6
Gabriel <rochagabriellima@gmail.com>
2021-07-21 16:54:27 -0300

    Reeditando o AppBar e adicionando Textos e seus TextFormFields

--- handson/lib/APP/UI/Telephone_Screen/Telephone.dart
+++ handson/lib/APP/UI/Telephone_Screen/Telephone.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:handson/APP/UI/Telephone_Screen/AppBar/AppBar.dart';
+import 'package:handson/APP/UI/Telephone_Screen/Widgets/MyTextFormField.dart';
 
 class Telephone extends StatelessWidget {
   Telephone({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MyBar();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.orange[300],
+        title: Text('Telefone'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_right_alt,
+              color: Colors.orange[300],
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Contato 1',
+              style: TextStyle(
+                color: Colors.orange[300],
+              ),
+            ),
+            Text(
+              'Contato 2',
+              style: TextStyle(
+                color: Colors.orange[300],
+              ),
+            ),
+            TextFormField(),
+          ],
+        ),
+      ),
+    );
   }
 }
